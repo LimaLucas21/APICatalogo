76b329c83d681ebfde64a5ba2c0febd1b50f533c
Lucas Lima <111473260+LimaLucas21@users.noreply.github.com>
2024-09-01 18:47:50 -0300

    Add files via upload

---
+++ APICatalogo/APICatalogo/Migrations/20240830181215_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categoria(Nome, ImagemUrl) Values('Bebidas','Bebidas.jpg')");
+            mb.Sql("Insert into Categoria(Nome, ImagemUrl) Values('Lanches','Lanches.jpg')");
+            mb.Sql("Insert into Categoria(Nome, ImagemUrl) Values('Sobremesas','Sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categoria");
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Migrations/20240830192740_PopulaProdutos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco,ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Coca-cola','Refrigerante de Cola 350 ml',5.45,'cocacola.jpg', 50, GETDATE(), 1)");
+            //mb.Sql("Insert into Produtos(Nome, ImagemUrl) Values('Lanches','Lanches.jpg')");
+            //mb.Sql("Insert into Produtos(Nome, ImagemUrl) Values('Sobremesas','Sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
